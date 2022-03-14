1410bdcdb20188c4596cb19b11f387005655fb28
Lee SeoJune <sung07288346@gmail.com>
2022-03-14 23:42:35 +0900

    📑 :: init

---
+++ finder-client/finder_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  finder_clientApp.swift
+//  finder-client
+//
+//  Created by 이서준 on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct finder_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
