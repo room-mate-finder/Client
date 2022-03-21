bf61b4840649209f75fd84ec00cab9275f1ae8e5
Lee SeoJune <sung07288346@gmail.com>
2022-03-21 23:44:06 +0900

    📑 :: LifeStyle enum

---
+++ finder-client/View/Type/LifeStyle.swift
@@ -0,0 +1,13 @@
+//
+//  LifeStyle.swift
+//  finder-client
+//
+//  Created by 이서준 on 2022/03/21.
+//
+
+import Foundation
+
+enum LifeStyle: String, CaseIterable {
+    case CLEAN = "깔끔"
+    case USEFUL = "실용"
+}
