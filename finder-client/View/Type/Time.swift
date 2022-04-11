0fa9f89b7516a19d953484d40f6e9871fb081b25
sung07288346@gmail.com <>
2022-04-11 10:57:10 +0900

    ⚡️ :: Time

--- finder-client/View/Type/Time.swift
+++ finder-client/View/Type/Time.swift
@@ -11,4 +11,15 @@ enum Time: String, CaseIterable {
     case EARLY = "일찍"
     case LATE = "늦게"
     case OTHER = "이 외"
+    
+    static func stringToTime(value: String) -> Time {
+        switch value {
+        case "LATE":
+            return Time.LATE
+        case "OTHER":
+            return Time.OTHER
+        default:
+            return Time.EARLY
+        }
+    }
 }
